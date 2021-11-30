005f4b90a323833368402a829c3b87a4d5fd5b2f
Boucher Alexis <82830827+Alexlemars@users.noreply.github.com>
2021-11-30 14:36:36 +0100

    Test unitaire bills 78%

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -4,17 +4,37 @@ import Bills  from "../containers/Bills.js"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { localStorageMock } from "../__mocks__/localStorage.js"
-import { ROUTES} from "../constants/routes"
+import Router from "../app/Router"
+import { ROUTES, ROUTES_PATH} from "../constants/routes"
 import firebase from "../__mocks__/firebase"
+import Firestore from "../app/Firestore"
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
+
+    
     test("Then bill icon in vertical layout should be highlighted", () => {
       const html = BillsUI({ data: []})
       document.body.innerHTML = html
-      //to-do write expect expression
+
+      Firestore.bills = () => ({ bills, get: jest.fn().mockResolvedValue() })
+
+      
+      Object.defineProperty(window, "localStorage", { value: localStorageMock })
+      window.localStorage.setItem("user", JSON.stringify({type: "Employee"}));
+
+      
+      const pathname = ROUTES_PATH["Bills"] // return '#employee/bills'
+      Object.defineProperty(window, "location", { value: { hash: pathname } })
+      
+      document.body.innerHTML = `<div id="root"></div>`
+      Router()
+
+      expect(screen.getByTestId("icon-window").classList.contains("active-icon")).toBe(true)
     })
+
+
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,11 +5,44 @@ import NewBill from "../containers/NewBill.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then it should render a New Bill form", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      
+
+      // Test la modal  
+      expect(screen.getByTestId('form-new-bill')).toBeTruthy()
     })
+
+    test("Then it should render 8 entrie",()=>{
+      const html = NewBillUI()
+      document.body.innerHTML = html
+
+      //Test le select type de depensse
+      expect(screen.getByTestId('expense-type')).toBeTruthy()
+      
+      //Test le nom de la depensse
+      expect(screen.getByTestId('expense-name')).toBeTruthy()
+
+      //Test la date de la depensse
+      expect(screen.getByTestId('datepicker')).toBeTruthy()
+
+      //Test le montant TTC
+      expect(screen.getByTestId('amount')).toBeTruthy()
+
+      //Test l'input de la TVA
+      expect(screen.getByTestId('vat')).toBeTruthy()
+
+      //Test l'input de la TVA
+      expect(screen.getByTestId('pct')).toBeTruthy()
+
+      //Test l'inpput du commentaire
+      expect(screen.getByTestId('commentary')).toBeTruthy()
+
+      //Test le le justificatif 
+      expect(screen.getByTestId('file')).toBeTruthy()
+    })
+
+    
   })
 })
 
